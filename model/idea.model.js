dcdbe3b4fd226695f336c5197bc9300b10111bb2
Monteiro3456 <123958151+Monteiro3456@users.noreply.github.com>
2023-06-18 21:36:00 +0530

    Add files via upload

---
+++ model/idea.model.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+      1 : {
+        "id": 1,
+        "idea_name" : "My great Idea",
+        "author name": "Vishwa",
+        "idea_description" : "Vishwa first gret idea"
+      }, 
+
+      2 : {
+        "id": 2,
+        "idea_name" : "My great Idea 2",
+        "author name": "Vishwa",
+        "idea_description" : "Vishwa second gret idea"
+      }
+
+
+
+}
